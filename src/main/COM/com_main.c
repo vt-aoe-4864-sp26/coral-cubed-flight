1d40e63a033daec585b315211e975aa300608a50
jackr25 <jackr25@vt.edu>
2026-03-23 17:51:18 -0400

    [COM} simple blink approach not working

--- src/main/COM/com_main.c
+++ src/main/COM/com_main.c
@@ -1,44 +1,19 @@
-// com_main.c
-// main application for the com board
-
-#include <stddef.h>
-#include <stdint.h>
 #include <zephyr/kernel.h>
-#include <com.h>
+#include <zephyr/drivers/gpio.h>
 
+static const struct gpio_dt_spec led =
+    GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
-// external function from com.c
-// extern void init_radio_tx_test(void);
+int main(void)
+{
+    if (!gpio_is_ready_dt(&led)) {
+        return -ENODEV;
+    }
+    gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
 
-// pin definitions
-
-int main(void) {
-  // setup mcu
-  //init_clock();
-  init_leds();
-  init_uart();
-  init_gpio();
-  //init_radio();cd 
-
-  // Blink on boot so we know the firmware started successfully!
-  com_blink_demo();
-
-  //blast_carrier();
-
-  // TAB initialization
-  rx_cmd_buff_t rx_cmd_buff = {.size=CMD_MAX_LEN, .route_id=COM};
-  clear_rx_cmd_buff(&rx_cmd_buff);
-  tx_cmd_buff_t tx_cmd_buff = {.size=CMD_MAX_LEN};
-  clear_tx_cmd_buff(&tx_cmd_buff);
-
-
-  // TAB loop - wait for commands
-  while(1) {
-    rx_uart(&rx_cmd_buff);             // Collect command bytes
-    reply(&rx_cmd_buff, &tx_cmd_buff); // Command reply logic
-    tx_uart(&tx_cmd_buff);             // Send a response if any
-  }
-  // Should never reach this point
-  return 0;
+    while (1) {
+        gpio_pin_toggle_dt(&led);
+        k_msleep(500);
+    }
+    return 0;
 }
-
